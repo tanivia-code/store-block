0864005df677e567d6a2a90d8a1c0e2268f17b5e
Tanivia Timbo <tanivia@gmail.com>
2020-08-14 16:48:38 -0300

    update

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -9,24 +9,31 @@ const CSS_HANDLES = ['countdown']
 const DEFAULT_TARGET_DATE = (new Date('2020-08-16')).toISOString()
 
 interface CountdownProps {
+  title: string,
   targetDate: string
  }
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ 
+  title,
+  targetDate, 
+}) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
     seconds: '00'
   })
 
+  const titleText = title || <FormattedMessage id="countdown.title" />
   const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime)
 
   return (
-  <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
-    { `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }
-  </div>
+    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
+      <div className={`${handles.title} db tc`}>{titleText}</div>
+      <div className={`${handles.countdown} db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+      </div>
+    </div>
   )
 }
 
