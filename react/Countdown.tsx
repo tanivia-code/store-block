833b3510a5b6c476c9eeff28a9bd92f254150673
Tanivia Timbo <tanivia@gmail.com>
2020-08-15 18:39:17 -0300

    update

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -3,22 +3,44 @@ import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
 import { useCssHandles } from 'vtex.css-handles'
 import { useQuery } from 'react-apollo'
-import { useProduct } from 'vtex.product-context/useProduct'
-import { productReleaseDate } from './queries/productReleaseDate.graphql'
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
 
 
 const CSS_HANDLES = ['countdown']
 
 const DEFAULT_TARGET_DATE = (new Date('2020-08-16')).toISOString()
 
-const { product: { 'red-analogic-coffee-and-tea-machine' } } = useProduct()
+const { product: { linkText } = useProduct()
+
 const { data, loading, error } = useQuery(productReleaseDate, {
    variables: {
-    slug: 'red-analogic-coffee-and-tea-machine', 
+    slug: linkText, 
    },
    ssr: false
-}),   
+})  
 
+if (loading) {
+  return (
+    <div>
+      <span>Loading...</span>
+    </div>
+  )
+}
+if (error) {
+  return (
+    <div>
+      <span>Erro!</span>
+    </div>
+  )
+}
+if (!product) {
+  return (
+    <div>
+      <span>Não há contexto de produto</span>
+    </div>
+  )
+}
 interface CountdownProps {
   targetDate: string
 }
