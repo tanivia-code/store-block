35d0848ef3d81a6235c5b886b4f184a9591bcf25
Tanivia Timbo <tanivia@gmail.com>
2020-08-15 21:48:42 -0300

    update

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -9,61 +9,56 @@ import productReleaseDate from './queries/productReleaseDate.graphql'
 const CSS_HANDLES = ['countdown']
 
 const DEFAULT_TARGET_DATE = (new Date('2020-08-16')).toISOString()
-
-const productContext = useProduct()
-const product = productContext?.product
-const linkText = product?.linkText
-
-const { data, loading, error } = useQuery(productReleaseDate, {
-   variables: {
-    slug: linkText, 
-   },
-   ssr: false
-})
-
-if (loading) {
-  return (
-    <div>
-      <span>Loading...</span>
-    </div>
-  )
-}
-if (error) {
-  return (
-    <div>
-      <span>Erro!</span>
-    </div>
-  )
-}
-if (!product) {
-  return (
-    <div>
-      <span>Não há contexto de produto</span>
-    </div>
-  )
-}
 interface CountdownProps {
   targetDate: string
 }
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-}) => {
-  const [
-    timeRemaining, 
-    setTime
-  ] = useState<TimeSplit>({
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
     seconds: '00'
   })
-  
-const handles = useCssHandles(CSS_HANDLES)
-
-tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
-
+  const handles = useCssHandles(CSS_HANDLES)
+  const { product } = useProduct()
+  if (typeof product === "undefined") {
+    tick(targetDate, setTime)
+  } else {
+    const { product: { linkText } } = useProduct()
+    const { data, loading, error } = useQuery(productReleaseDate, {
+      variables: {
+        slug: linkText
+      },
+      ssr: false
+    })
+    if (loading) {
+      return (
+        <div>
+          <span>Loading...</span>
+        </div>
+      )
+    }
+    if (error) {
+      return (
+        <div>
+          <span>Erro!</span>
+        </div>
+      )
+    }
+    if (!product) {
+      return (
+        <div>
+          <span>Não há contexto de produto</span>
+        </div>
+      )
+    }
+    tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+  }
   return (
+    <div>
       <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
         {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
       </div>
+    </div>
   )
 }
 
